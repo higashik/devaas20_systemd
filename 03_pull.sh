32e55516621d659843b4d860aff8dee9246352e6
Kazuhiko HIGASHI <kazuhiko.higashi@nttcom.co.jp>
2018-06-08 15:32:43 +0900

    docker login when pulling

--- 03_pull.sh
+++ 03_pull.sh
@@ -4,6 +4,8 @@ postgres=registry.gitlab.com/kazuhcurrycw/test/postgres:dap_9.5.5_app
 tomcat=registry.gitlab.com/kazuhcurrycw/test/tomcat:dap_8.5.15_app
 httpd=registry.gitlab.com/kazuhcurrycw/test/httpd:dap_2.4.6_app
 
+docker login registry.gitlab.com
+
 date
 docker pull $postgres
 date
